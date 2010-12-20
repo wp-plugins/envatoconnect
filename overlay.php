6a69bca95c2c7ad317df370e4e79dd593e66c612
wizylabs <wizylabs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-20 15:44:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/envatoconnect/tags/1.0@324654 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ overlay.php
@@ -0,0 +1,66 @@
+<div id="envatoconnect_wrap">
+
+	<h2 class="envatoconnect_heading login">Connect with Envato Marketplace</h2>
+	
+	<div class="envatoconnect_login_form">
+	
+		<form action="" id="envatoconnect_form" method="post">
+			
+			<ul class="envatoconnect_form_wrap">
+				<li class="envatoconnect_username">
+					<label for="envatoconnect_username">Username:</label>
+					<input type="text" name="envatoconnect_username" id="envatoconnect_username" class="envatoconnect_text required">
+				</li>
+				<li class="envatoconnect_api">
+					<label for="envatoconnect_api">API Key:</label>
+					<input type="text" name="envatoconnect_api" id="envatoconnect_api" class="envatoconnect_text required">
+				</li>
+				<li class="envatoconnect_login">
+					<input type="submit" value="Sign In" name="envatoconnect_login" id="envatoconnect_login">
+					<input type="hidden" name="envatoconnect" value="signin">
+					<input type="hidden" name="action" value="envatoconnect_ajax_post_action">
+				</li>
+			</ul>
+			
+		</form>
+	
+	</div>
+	<!-- /.envatoconnect_login_form -->
+	
+	<div class="envatoconnect_signup">
+	
+		<h2 class="envatoconnect_heading signup">Need an account?</h2>
+		
+		<form action="" id="envatoconnect_signup" method="post">
+		
+			<ul class="envatoconnect_form_wrap">
+				<li class="envatoconnect_marketplace">
+					<label for="envatoconnect_marketplace">Select Marketplace</label>
+					<select name="envatoconnect_marketplace" id="envatoconnect_marketplace" class="envatoconnect_select">
+						<option value="http://themeforest.net/signup">ThemeForest</option>
+						<option value="http://codecanyon.net/signup">CodeCanyon</option>
+						<option value="http://audiojungle.net/signup">AudioJungle</option>
+						<option value="http://graphicriver.net/signup">GraphicRiver</option>
+						<option value="http://activeden.net/signup">ActiveDen</option>
+						<option value="http://videohive.net/signup">VideoHive</option>
+						<option value="http://3docean.net/signup">3DOcean</option>
+						<option value="http://marketplace.tutsplus.com/signup">Tuts+ Marketplace</option>
+					</select>
+				</li>
+				<li class="envatoconnect_signup_go">
+					<input type="submit" value="Sign Up!" name="envatoconnect_signup" id="envatoconnect_signup">
+					<input type="hidden" name="envatoconnect" value="signup">
+				</li>
+			</ul>
+		
+		</form>
+	
+	</div>
+	<!-- /.envatoconnect_signup -->
+	
+	<div class="clear"></div>
+	
+	<div class="pbea"></div>
+
+</div>
+<!-- /#envatoconnect_wrap -->
